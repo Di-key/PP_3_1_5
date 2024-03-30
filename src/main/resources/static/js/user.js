03ce2a0cc2b514faa6325726d1d63393806ccff9
Di-key <vfantomkhayv@mail.ru>
2024-03-30 11:44:14 +0300

    commit

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,4 +1,28 @@
 let currentUser = "";
+let tableUser = [];
+let request = new Request("/api/users/current", {
+    method: "GET",
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+getUser()
+
+function getUser() {
+    fetch(request).then(res =>
+        res.json()).then(data => {
+        tableUser = [];
+        if (data.length > 0) {
+            data.forEach(user => {
+                tableUser.push(user)
+            })
+        } else {
+            tableUser = [];
+        }
+
+        showOneUser(tableUser);
+    })
+}
 
 fetch("/api/users/current").then(res => res.json())
     .then(data => {
